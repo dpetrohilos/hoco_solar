5ea84677d546ead297e52c8f5567d8b273ed15f2
dpetrohilos <dylanpetrohilos@gmail.com>
2022-03-23 14:38:07 -0400

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,31 @@
-(function( root, $, undefined ) {
-	"use strict";
+// lazy load front page images 
 
-	$(function () {
-		// DOM ready, take it away
-	});
+const io = new IntersectionObserver((entries) =>
+            entries.forEach((entry) => {
+                // set image source only when it is in the viewport
+                if (entry.isIntersecting) {
+                    const image = entry.target;
+                    // setting image source from the dataset
+                    image.src = image.dataset.src;
 
-} ( this, jQuery ));
+                    // when image is loaded, we do not need to observe it any more
+                    io.unobserve(image);
+                }
+            })
+        );
+
+        document.querySelectorAll(".lazy").forEach((element) => io.observe(element));
+
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape"){
+  document.getElementById("menu-bar").classList.remove("change");
+  document.getElementById("nav").classList.remove("change");
+  document.getElementById("menu-bg").classList.remove("change-bg");
+	}
+});
