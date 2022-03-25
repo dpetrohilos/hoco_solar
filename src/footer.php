2f45e694b9a03e5468bb6e2a50125ddf905da97d
dpetrohilos <dylanpetrohilos@gmail.com>
2022-03-24 22:04:24 -0400

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -24,7 +24,7 @@
                 
                 <!-- copyright -->
 			    <?php
-				    printf( '<p class="copyright">' . __( '&copy; %1$s Copyright %2$s. Powered by <a href="%3$s" title="WordPress">WordPress</a> &amp; <a href="%4$s" title="HTML5 Blank">HTML5 Blank</a>.', 'html5blank' ) . '</p>',
+				    printf( '<p class="copyright">' . __( '&copy; %1$s Copyright %2$s. Powered by <a href="%3$s" title="WordPress">WordPress</a> &amp; <a href="%4$s" title="HTML5 Blank">HTML5 Blank</a>.', 'html5blank' ) . 'Designed by <a href="repeater.digital" title="link to Repeater Digital Site">Repeater.Digital</p>',
 				    	date( 'Y'),
 				    	esc_html( get_bloginfo( 'name' ) ),
 				    	'//wordpress.org',
