22c583036b390eaa656b40ecdbd9f31143739bd5
dpetrohilos <dylanpetrohilos@gmail.com>
2022-03-24 20:58:25 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -12,14 +12,9 @@
 <img height="1200" width="1200" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img-1.jpg" alt="Beautiful Photo Of A Solar Farm">
 
             
-            <figcaption><h2>Howard County's Most Affordable Solar Farm</h2> </figcaption>
-            <figcaption>Est. 2022</figcaption>
-            <figcaption>
             
-                <img height="1200" width="1200" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/hoco" alt="Hoco " class="logo-img">
 
                 
-            </figcaption>
 
 
             
@@ -33,7 +28,12 @@
 		</div>
 		<!-- /section -->
 	
-
+        <div>      <h2>Howard County's Most Affordable Solar Farm</h2> 
+</div>
+        <div>            <p>Est. 2022</p>
+        </div>
+        <div>                <img height="1200" width="1200" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/hoco" alt="Hoco " class="logo-img">
+</div>
 
 </main>
 
