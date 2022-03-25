ca5688c5673ccf0f0322834361bdbbe0593f810d
dpetrohilos <dylanpetrohilos@gmail.com>
2022-03-24 20:47:14 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,11 +4,22 @@
         
         <div>
             
+            
+            
         <figure>
+            
+    
+<img height="1200" width="1200" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img-1.jpg" alt="Beautiful Photo Of A Solar Farm">
+
             
             <figcaption><h2>Howard County's Most Affordable Solar Farm</h2> </figcaption>
             <figcaption>Est. 2022</figcaption>
-            <figcaption></figcaption>
+            <figcaption>
+            
+                <img height="1200" width="1200" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/hoco" alt="Hoco " class="logo-img">
+
+                
+            </figcaption>
 
 
             
@@ -16,7 +27,7 @@
         
         
 		<!-- section -->
-		<div>
+		<div class="light">
 
 
 		</div>
@@ -27,12 +38,32 @@
 </main>
 
 
-<div class="grid grid-item-2">
+<div class="grid grid-item-3">
     
-    <div></div>
-    <div></div>
-    <div><?php get_sidebar(); ?></div>
-    <div></div>
+    <div class="red">
+        <h3>Locally Sourced</h3>
+        
+    </div>
+    <div class="orange">
+            <h4>Our Future</h4>
+
+    
+    </div>
+    <div class="sidebar"><?php get_sidebar(); ?></div>
+    <div>
+    
+    <figure>
+            
+    
+<img height="1200" width="1200" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img-2.jpg" alt="Beautiful Photo Of A Solar Farm">
+
+            
+            <figcaption><h4>Solar Energy Independence</h4> </figcaption>
+        
+
+            
+            </figure>
+    </div>
 
 
 </div>
