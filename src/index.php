062ec2bcc88bb83ee3cdb3562347bf3904384365
dpetrohilos <dylanpetrohilos@gmail.com>
2022-03-24 21:40:31 -0400

    Beta update

--- src/index.php
+++ src/index.php
@@ -23,11 +23,13 @@
 		<!-- section -->
 		<div class="light">
 
+            
+            <img height="322" width="122" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/HC-Logo.svg" alt="Hoco " class="logo-img">
 
 		</div>
 		<!-- /section -->
 	
-        <div>      <h2>Howard County's Most Affordable Solar Farm</h2> 
+        <div>      <h2 class="intro">Howard County's Most Affordable Solar Farm</h2> 
 </div>
         <div>            <p>Est. 2022</p>
         </div>
@@ -40,28 +42,30 @@
 <div class="grid grid-item-3">
     
     <div class="red">
-        <h3>Locally Sourced</h3>
+        <h3 class="headline">Locally Sourced</h3>
         
     </div>
     <div class="orange">
-            <h4>Our Future</h4>
+            <h4 class="headline">Our Future</h4>
 
     
     </div>
     <div class="sidebar"><?php get_sidebar(); ?></div>
     <div>
     
-    <figure>
             
     
 <img height="1200" width="1200" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img-2.jpg" alt="Beautiful Photo Of A Solar Farm">
 
             
-            <figcaption><h4>Solar Energy Independence</h4> </figcaption>
         
 
             
-            </figure>
+    </div>
+    <div>
+    
+           <h5 class="headline">Solar Energy Independence</h4> 
+
     </div>
 
 
