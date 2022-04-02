57760e917350f0335f221038ce92431e36d202cc
dpetrohilos <dylanpetrohilos@gmail.com>
2022-04-02 16:20:32 -0400

    the fashion zombies

--- src/footer.php
+++ src/footer.php
@@ -12,7 +12,7 @@
                 </nav>
                 
                 
-                <div class="grid-item-3 grid-item changa"><h6 class="intro ">Questions?</h6>
+                <div class="grid-item-3 grid-item changa"><h6 class="intro top">Questions?</h6>
                     <p>Info at hocosolarfarm dot com</p>
                 
                 </div>
