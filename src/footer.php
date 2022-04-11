6d9a1f1b958ecbd9bebc4e8584a156d24116a787
dpetrohilos <dylanpetrohilos@gmail.com>
2022-04-11 15:39:02 -0400

    grid item update

--- src/footer.php
+++ src/footer.php
@@ -30,7 +30,7 @@
 </a>
 
     </div></div>
-                <div class="grid-item-5 changa content grid-item">
+                <div class="grid-item-5 changa content grid-item v-center h-center">
                 <!-- copyright -->
 			    <?php
 				    printf( '<p class="copyright changa ">' . __( '&copy; %1$s Copyright %2$s. Powered by <a href="%3$s" title="WordPress">WordPress</a> &amp; <a href="%4$s" title="HTML5 Blank">HTML5 Blank</a>.', 'html5blank' ) . ' Designed by <a href="repeater.digital" title="link to Repeater Digital Site">Repeater.Digital</p>',
