a4facec39e23ff8d01e279c5b2df542d63fde2b2
dpetrohilos <dylanpetrohilos@gmail.com>
2022-04-11 21:22:23 -0400

    function fix 1

--- src/functions.php
+++ src/functions.php
@@ -121,63 +121,7 @@ function html5blank_nav() {
     );
 }
 
-// Load HTML5 Blank scripts (header.php)
-function html5blank_header_scripts() {
-    if ( $GLOBALS['pagenow'] != 'wp-login.php' && ! is_admin() ) {
-        if ( HTML5_DEBUG ) {
-            // jQuery
-            wp_deregister_script( 'jquery' );
 
-            // Conditionizr
-
-            // Modernizr
-            wp_register_script( 'modernizr', get_template_directory_uri() . '/js/lib/modernizr.js', array(), '2.8.3' );
-
-            // Custom scripts
-            wp_register_script(
-                'html5blankscripts',
-                get_template_directory_uri() . '/js/scripts.js',
-                array(
-                    'modernizr',
-                ),
-                '1.0.0' );
-
-            // Enqueue Scripts
-            wp_enqueue_script( 'html5blankscripts' );
-
-        // If production
-        } else {
-            // Scripts minify
-            wp_register_script( 'html5blankscripts-min', get_template_directory_uri() . '/js/scripts.min.js', array(), '1.0.0' );
-            // Enqueue Scripts
-            wp_enqueue_script( 'html5blankscripts-min' );
-        }
-    }
-}
-
-// Load HTML5 Blank conditional scripts
-
-
-
-
-// Load HTML5 Blank styles
-function html5blank_styles() {
-    if ( HTML5_DEBUG ) {
-        // normalize-css
-        wp_register_style( 'normalize', get_template_directory_uri() . '/css/lib/normalize.css', array(), '7.0.0' );
-
-        // Custom CSS
-        wp_register_style( 'html5blank', get_template_directory_uri() . '/style.css', array( 'normalize' ), '1.0' );
-
-        // Register CSS
-        wp_enqueue_style( 'html5blank' );
-    } else {
-        // Custom CSS
-        wp_register_style( 'html5blankcssmin', get_template_directory_uri() . '/style.css', array(), '1.0' );
-        // Register CSS
-        wp_enqueue_style( 'html5blankcssmin' );
-    }
-}
 
 // Register HTML5 Blank Navigation
 function register_html5_menu() {
