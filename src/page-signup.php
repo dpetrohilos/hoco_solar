89c6aabc31f22ea2273b046c930d6120f419e04e
dpetrohilos <dylanpetrohilos@gmail.com>
2022-04-12 14:45:03 -0400

    sign up page

---
+++ src/page-signup.php
@@ -0,0 +1,12 @@
+<?php /* Template Name: signup */ get_header(); ?>
+
+<div class="grid-wrapper-internal-top grid-area" area-label="content" role="heading">
+
+
+	<!-- /section -->
+
+<?php get_sidebar(); ?>
+
+</div>    
+    
+<?php get_footer(); ?>
