3d1e75b32f5b338385061eb097e03ca53a4f9b2b
dpetrohilos <dylanpetrohilos@gmail.com>
2022-04-12 13:09:05 -0400

    update

--- src/index.php
+++ src/index.php
@@ -3,10 +3,17 @@
 <main role="main" aria-label="Content" class="grid grid-wrapper-2">
 <div class="grid-item-1 grid-item text-wrapper">
     
-    <div><img height="1200" width="2400" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img-1.jpg" alt="Beautiful Image of a Solar Farm"></div>
+<div>
+    
+<img height="1200" width="2400" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img-1.jpg" alt="Beautiful Image of a Solar Farm">
+    
+</div>
   
-<div class="content"><p class="w">Est. 2022</p>
+<div class="content">
+    
+<p class="w">Est. 2022</p>
 <h1 class="intro w">Howard County's Most Affordable Solar Farm</h1>    
+
 </div>
     
     <div><svg id="a" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 326.84 84.48"><g id="b" transform="translate(1 111)" style="isolation:isolate;"><g style="isolation:isolate;"><path d="M55.36,52.29H17.72v33.46H.76V6.05H17.72v31.31H55.36V6.05h17.09V85.75h-17.09V52.29Z" style="fill:none; stroke:#fff;"/><path d="M120.72,4.74c24.14,0,41.7,17.21,41.7,41.22s-17.56,41.22-41.7,41.22-41.58-17.21-41.58-41.22S96.7,4.74,120.72,4.74Zm0,15.05c-14.7,0-24.14,11.23-24.14,26.17s9.44,26.17,24.14,26.17,24.26-11.35,24.26-26.17-9.56-26.17-24.26-26.17Z" style="fill:none; stroke:#fff;"/><path d="M165.41,45.96c0-24.49,18.52-41.22,41.82-41.22,18.16,0,28.32,9.56,33.93,19.48l-14.58,7.17c-3.35-6.45-10.51-11.59-19.36-11.59-13.86,0-24.38,11.11-24.38,26.17s10.52,26.17,24.38,26.17c8.84,0,16.01-5.02,19.36-11.59l14.58,7.05c-5.74,9.92-15.77,19.59-33.93,19.59-23.3,0-41.82-16.73-41.82-41.22Z" style="fill:none; stroke:#fff;"/><path d="M283.94,4.74c24.14,0,41.7,17.21,41.7,41.22s-17.56,41.22-41.7,41.22-41.58-17.21-41.58-41.22,17.56-41.22,41.58-41.22Zm0,15.05c-14.7,0-24.14,11.23-24.14,26.17s9.44,26.17,24.14,26.17,24.26-11.35,24.26-26.17-9.56-26.17-24.26-26.17Z" style="fill:none; stroke:#fff;"/></g></g></svg></div>
