30f17121b8117561e7aee9edbfca368abcc72d38
dpetrohilos <dylanpetrohilos@gmail.com>
2022-04-22 00:31:45 -0400

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -23,6 +23,12 @@
 <div class="bottom-footer-row">
 
 <div class="grid-item-1 changa content grid-item">
+    
+    <p class="subhead">About HoCo Solar Farm</p>
+    <p>Hoco Solar Farm is a collaboration between Utility Advisor, Maxed Out Solar, and Repeater Digital. We created Hoco Solar to make it easier for Howard County residents to transition to clean energy. <strong>#GoSolarMD</strong></p>
+    <p class="subhead">What other's are saying</p>
+    <blockquote>An enthusiastic advocate for solar energy, Kevin Cheung has done great work for the Howard County Sierra Club... These community solar projects are great way to meet that commitment while bringing in tax revenue for Howard County.</blockquote> 
+    <cite>— Carolyn Parsa, Sierra Club Howard County Chair</cite>
     <p>&copy; <?php echo date("Y"); ?> Copyright Howard County Solar Farm. Powered by <a href="www.wordpress.org" title="link to wordpress site">WordPress</a> &amp; <a href="http://www.html5blank.com/" title="link to html5 blank">HTML5 Blank.</a> Designed by <a href="https://www.repeater.digital" title="link to Repeater Digital Site">Repeater.Digital</a></p>
 
 
