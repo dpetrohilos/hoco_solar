8cde87cfaed38bef479251def3f2926705f68ca3
dpetrohilos <dylanpetrohilos@gmail.com>
2022-04-22 00:27:58 -0400

    Update page-signup.php

--- src/page-signup.php
+++ src/page-signup.php
@@ -1,7 +1,10 @@
 <?php /* Template Name: signup */ get_header(); ?>
 
 <div class="grid-wrapper-internal-top grid-area" area-label="content" role="heading">
-
+    <h1>Confirm you're a Howard County resident</h1>
+    <h2 class="subhead">
+    We'll send you $50 for confirming you're a Howard County resident. (If you're not from Hoco that's cool too.)
+    </h2>
 
 	<!-- /section -->
 
